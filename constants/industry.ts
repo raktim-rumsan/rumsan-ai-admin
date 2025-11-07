e6506ab70b99758ddf5764e6ad4ddcd32d0ecf14
nabina-rumsan <nabina@rumsan.net>
2025-11-07 14:56:48 +0545

    removed industry.ts

--- constants/industry.ts
+++
@@ -1,6 +0,0 @@
-const values = process.env.NEXT_PUBLIC_INDUSTRY_VALUES?.split(",").map(i => i.trim()) || [];
-
-export const INDUSTRY_OPTIONS = values.map(value => ({
-  label: value.charAt(0).toUpperCase() + value.slice(1),
-  value,
-}));
