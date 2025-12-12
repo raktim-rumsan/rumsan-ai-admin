6aea2f675be90d84d9957e4d9dc781f98ddad4db
Akash-rumsan <akash.sunar@rumsan.net>
2025-12-12 15:53:54 +0545

    fix/image rendering process and back button issue

--- app/dashboard/(sections)/workspace/[workspaceId]/integrations/[slug]/page.tsx
+++ app/dashboard/(sections)/workspace/[workspaceId]/integrations/[slug]/page.tsx
@@ -1,28 +1,22 @@
 "use client";
 import { integrationItem } from "@/components/sections/integrations/integration-constant";
 import { Button } from "@/components/ui/button";
-import { notFound, useRouter } from "next/navigation";
-import { use } from "react";
+import { notFound, useParams, useRouter } from "next/navigation";
 
-export default function IntegrationDetail({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
+export default function IntegrationDetail() {
   const router = useRouter();
-  const resolvedParams = use(params);
-  const item = integrationItem.slugsItems.find(
-    (item) => item.slug === resolvedParams.slug
-  );
+  const { slug, workspaceId } = useParams();
 
+  const item = integrationItem.slugsItems.find((item) => item.slug === slug);
   if (!item) return notFound();
-
   return (
     <div className="p-4 w-full max-w-full space-y-6 overflow-hidden">
       <div className="flex items-center gap-4">
         <Button
           variant="ghost"
-          onClick={() => router.push("/dashboard/integrations")}
+          onClick={() =>
+            router.push(`/dashboard/workspace/${workspaceId}/integrations`)
+          }
           className="text-gray-600 hover:text-gray-900"
         >
           ← Back to Integrations
